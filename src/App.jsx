e5f26d7e9dfb8fe07a3eb7ab44c024568a60a3f1
Siddhesh-wagh <siddhesh.01092004@gmail.com>
2025-08-12 20:49:27 +0530

    view more added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProjectsPage from "./pages/ProjectsPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
     </>
   );
